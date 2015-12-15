3c2275e56e498708fb11c260f6cd9315e66ade8d
Phil Nash <philnash@gmail.com>
2015-12-15 11:52:44 +0000

    Included all messages in conversation view

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ function hideNumbers(messages) {
     numberMappings[shasum] = message.from;
     message.from = shasum;
   };
+  return messages;
 }
 
 function numToSha(number){
@@ -21,19 +22,46 @@ function numToSha(number){
   return shasum;
 }
 
+var date_sort_asc = function (date1, date2) {
+  // This is a comparison function that will result in dates being sorted in
+  // ASCENDING order. As you can see, JavaScript's native comparison operators
+  // can be used to compare dates. This was news to me.
+  if (date1 > date2) return 1;
+  if (date1 < date2) return -1;
+  return 0;
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   twilio.messages.list({to: process.env.TWILIO_PHONE_NUMBER}).then(function(data){
-    hideNumbers(data.messages);
-    res.render('index', { title: 'Inboks', messages: data.messages });
+    var incomingMessages = hideNumbers(data.messages);
+    res.render('index', { title: 'Inboks', messages: incomingMessages });
   });
 });
 
 router.get('/:sha', function(req, res, next){
   var number = numberMappings[req.params.sha];
+  var incomingMessages;
   twilio.messages.list({from: number, to: process.env.TWILIO_PHONE_NUMBER}).then(function(data){
-    hideNumbers(data.messages);
-    res.render('conversation', { title: 'Inboks', messages: data.messages, sha: data.messages[0].from });
+    incomingMessages = hideNumbers(data.messages);
+    return twilio.messages.list({from: process.env.TWILIO_PHONE_NUMBER, to: number});
+  }).then(function(data){
+    var outgoingMessages = data.messages;
+
+    var allMessages = incomingMessages.concat(outgoingMessages);
+    allMessages.sort(function(a, b){
+      return date_sort_asc(Date.parse(a.dateCreated), Date.parse(b.dateCreated));
+    });
+    allMessages = allMessages.map(function(message){
+      message.reply = message.from !== process.env.TWILIO_PHONE_NUMBER;
+      return message;
+    });
+
+    res.render('conversation', {
+      title: 'Inboks',
+      messages: allMessages,
+      sha: data.messages[0].from
+    });
   });
 });
 
