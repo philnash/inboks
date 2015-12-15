3b534d6c22ff9bbe4b53323d9b1c11c8c5e5abcb
Phil Nash <philnash@gmail.com>
2015-12-15 10:40:11 +0000

    Simple list of messages and response

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('dotenv').load();
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var twilio = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Inboks' });
+  twilio.messages.list({to: process.env.TWILIO_PHONE_NUMBER}).then(function(data){
+    res.render('index', { title: 'Inboks', messages: data.messages });
+  });
+});
+
+router.post('/messages', function(req, res, next){
+  var twiml = "<Response><Message>Hi, thanks for joining my stream today!</Message></Response>";
+  res.set('Content-Type', 'text/xml');
+  res.send(twiml);
 });
 
 module.exports = router;
